a0d5e8e461ad6950012c71c479e6b8785863fb1c
ponzis <ponzis.online@gmail.com>
2018-09-18 17:41:27 -0400

    fixed tox and travis

--- setup.py
+++ setup.py
@@ -2,20 +2,11 @@ from setuptools import setup
 
 setup(
     name='Tilphan',
-    version='0.0.1',
-    packages=['tilphan', 'tests'],
+    version='',
+    packages=['tests', 'tilphan'],
     url='',
     license='',
     author='Ponzis',
     author_email='',
-    description='',
-    extras_require={
-        'docs': [
-            'sphinx',
-            'sphinx_rtd_theme',
-        ],
-        'testing': [
-            'pytest',
-        ],
-    }
+    description=''
 )

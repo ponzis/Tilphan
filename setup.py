bc5d88a793bca8c5092dcdb2912839d8424a85dc
ponzis <ponzis.online@gmail.com>
2018-09-18 14:16:31 -0400

    Adding tox support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tilphan',
+    version='0.0.01',
+    packages=['tilphan'],
+    url='',
+    license='',
+    author='Ponzis',
+    author_email='',
+    description=''
+)
---
+++ tests/plugin_test_test.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestTest(TestCase):
+    def test_on_yay(self):
+        self.fail()

51521993689ddbb5ab4730caf90f1fafe7e7696a
ponzis <ponzis.online@gmail.com>
2018-09-18 18:21:42 -0400

    fixed tox and travis

--- setup.py
+++ setup.py
@@ -8,5 +8,18 @@ setup(
     license='',
     author='Ponzis',
     author_email='',
-    description=''
+    description='',
+    python_requires='>=3.5.3',
+    install_requires=[
+        'discord.py[voice]>=0.16.12',
+
+    ],
+    extras_require={
+        'docs': [
+            'sphinx>=1.7.4',
+            'sphinxcontrib-asyncio',
+            'sphinxcontrib-websupport',
+        ]
+    }
+
 )
